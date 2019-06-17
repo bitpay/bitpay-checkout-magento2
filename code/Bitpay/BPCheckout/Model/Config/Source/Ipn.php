b17ce021815f1e205b6552aadc490bef4255562a
Joshua Lewis <joshlewis@gmail.com>
2019-06-17 13:08:05 -0400

    IPN Order Mapping Added IPN order mapping settings

---
+++ code/Bitpay/BPCheckout/Model/Config/Source/Ipn.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Bitpay\BPCheckout\Model\Config\Source;
+
+
+/**
+ * IPN Model
+ */
+class Ipn implements \Magento\Framework\Option\ArrayInterface
+{
+
+    public function toOptionArray()
+    {
+        return [
+            'pending' => 'Pending Payment',
+            'processing' => 'Processing',
+        ];
+
+    }
+}
