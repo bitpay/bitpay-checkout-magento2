cd0f465b0bfd5521e8a7777d4c8e4599662bd855
Joshua Lewis <joshlewis@gmail.com>
2019-06-20 10:56:39 -0400

    order details page

--- code/Bitpay/BPCheckout/view/frontend/web/js/bitpay/config.js
+++ code/Bitpay/BPCheckout/view/frontend/web/js/bitpay/config.js
@@ -1,5 +1,5 @@
 function showModal(env, response) {
-   
+
     if (env == 'test') {
         bitpay.enableTestMode()
     }
@@ -61,22 +61,25 @@ function getCookie(cname) {
 }
 if (window.location.href.indexOf("checkout/onepage/success/") > -1 && getCookie('modal') == 1) {
     setTimeout(function () {
-        showModal(getCookie('env'), getCookie('invoicedata'))    
+        showModal(getCookie('env'), getCookie('invoicedata'))
     }, 750);
-   
+
 }
 //guest checkout 
 //autofill the guest info
-if(window.location.pathname.indexOf('sales/guest/form') != -1){
-    //autofill form
-    setTimeout(function(){ 
-      jQuery("#oar-order-id").val(getCookie("oar_order_id"))
-      jQuery("#oar-billing-lastname").val(getCookie("oar_billing_lastname"))
-      jQuery("#oar_email").val(getCookie("oar_email"))
-      deleteCookie("oar_order_id")
-      deleteCookie("oar_billing_lastname")
-      deleteCookie("oar_email")
 
-    }, 
-      1500);  
-  }
+
+if (window.location.pathname.indexOf('sales/guest/form') != -1) {
+    //autofill form
+    if (document.cookie.indexOf('oar_order_id') !== -1) {
+        setTimeout(function () {
+                jQuery("#oar-order-id").val(getCookie("oar_order_id"))
+                jQuery("#oar-billing-lastname").val(getCookie("oar_billing_lastname"))
+                jQuery("#oar_email").val(getCookie("oar_email"))
+                deleteCookie("oar_order_id")
+                deleteCookie("oar_billing_lastname")
+                deleteCookie("oar_email")
+            },
+            1500);
+    }
+}
