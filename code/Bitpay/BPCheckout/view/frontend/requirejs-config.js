e5c95855a5c5c6a867ad1d2acc460cce2d765b59
Wojciech Kogut <w.kogut@sumoheavy.com>
2022-08-17 17:08:15 +0200

    SP-136 JS & jQuery Cleanup

---
+++ code/Bitpay/BPCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            bitpay: 'https://bitpay.com/bitpay.min.js',
+        }
+    }
+};
--- code/Bitpay/BPCheckout/view/frontend/web/js/bitpay/checkout/index.js
+++ code/Bitpay/BPCheckout/view/frontend/web/js/bitpay/checkout/index.js
@@ -1,6 +1,7 @@
 define([
     'jquery',
-    'mage/mage'
+    'mage/mage',
+    'bitpay'
 ], function (jQuery) {
     'use strict';
 
