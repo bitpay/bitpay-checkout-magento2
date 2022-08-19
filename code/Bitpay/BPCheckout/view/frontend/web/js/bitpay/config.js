dcb9634b1d6020c8aa68927d152c3e9175634989
Wojciech Kogut <w.kogut@sumoheavy.com>
2022-08-19 15:19:17 +0200

    SP-137 Data from Cookies

--- code/Bitpay/BPCheckout/view/frontend/web/js/bitpay/config.js
+++ code/Bitpay/BPCheckout/view/frontend/web/js/bitpay/config.js
@@ -1,39 +1,3 @@
-if (window.location.pathname.indexOf('sales/guest/form') != -1) {
-
-    //autofill form
-    if (document.cookie.indexOf('oar_order_id') !== -1) {
-        $guest = getCookie("guest")
-
-        setTimeout(function () {
-                jQuery("#oar-order-id").val(getCookie("oar_order_id"))
-                jQuery("#oar-billing-lastname").val(getCookie("oar_billing_lastname"))
-                jQuery("#oar_email").val(getCookie("oar_email"))
-                deleteCookie("oar_order_id")
-                deleteCookie("oar_billing_lastname")
-                deleteCookie("oar_email")
-            },
-            2000);
-    }
-}
-if (window.location.pathname == "/checkout/") {
-    //try and load info from cookie
-    setTimeout(function () {
-        var $checkC = getCookie("buyer_first_name")
-        if(typeof $checkC != undefined && $checkC != ""){
-        jQuery('input[name="firstname"]').val(getCookie("buyer_first_name").replace(/\+/g, ' '))
-        jQuery('input[name="lastname"]').val(getCookie("buyer_last_name").replace(/\+/g, ' '))
-        jQuery('input[name="street[0]"]').val(getCookie("buyer_street").replace(/\+/g, ' '))
-        jQuery('input[name="city"]').val(getCookie("buyer_city").replace(/\+/g, ' '))
-        jQuery('input[name="postcode"]').val(getCookie("buyer_postcode").replace(/\+/g, ' '))
-        jQuery('input[name="telephone"]').val(getCookie("buyer_telephone").replace(/\+/g, ' '))
-        jQuery('#customer-email').val(getCookie("buyer_email").replace(/\+/g, ' '))
-	
-        }
- },
- 3500);
-}
-
-
 function getParams() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -41,21 +5,7 @@ function getParams() {
     });
     return vars;
 }
-function getCookie(cname) {
-var name = cname + "=";
-var decodedCookie = decodeURIComponent(document.cookie);
-var ca = decodedCookie.split(';');
-for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-    }
-}
-return "";
-}
+
 function deleteCookie(cname) {
     document.cookie = cname + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;';
 }
