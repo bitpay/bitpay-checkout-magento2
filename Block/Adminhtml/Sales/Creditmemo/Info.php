26ce218b3c858bb6524cffc5e788f240ca7c97e4
Wojciech Kogut <w.kogut@sumoheavy.com>
2023-03-22 12:54:44 +0100

    SP-397 fix err on credit memo view, fix partial refund

--- Block/Adminhtml/Sales/Creditmemo/Info.php
+++ Block/Adminhtml/Sales/Creditmemo/Info.php
@@ -16,25 +16,37 @@ class Info extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
 
     protected $priceCurrency;
 
+    protected $config;
+
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Framework\Registry $registry,
         \Magento\Sales\Helper\Admin $adminHelper,
         BitpayRefundRepository $bitpayRefundRepository,
         PriceCurrency $priceCurrency,
+        Config $config,
         array $data = [],
         ?ShippingHelper $shippingHelper = null,
         ?TaxHelper $taxHelper = null
     ) {
         $this->bitpayRefundRepository = $bitpayRefundRepository;
+        $this->config = $config;
         $this->priceCurrency = $priceCurrency;
         parent::__construct($context, $registry, $adminHelper, $data, $shippingHelper, $taxHelper);
     }
 
     public function getBitpayAdditionalInfo(): array
     {
+        if (!$this->config->isPaymentActive()) {
+            return [];
+        }
+
         $creditmemo = $this->getCreditmemo();
         $order = $creditmemo->getOrder();
+        if ($this->isBitpayPaymentMethod($order)) {
+            return [];
+        }
+
         $refundData = $this->bitpayRefundRepository->getByOrderId($order->getId());
         $amount = $this->priceCurrency->format($refundData['amount']);
 
@@ -56,4 +68,12 @@ class Info extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
         return $this->_coreRegistry->registry('current_creditmemo');
     }
 
+    /**
+     * @param \Magento\Sales\Model\Order $order
+     * @return bool
+     */
+    protected function isBitpayPaymentMethod(\Magento\Sales\Model\Order $order): bool
+    {
+        return $order->getPayment()->getMethod() !== Config::BITPAY_PAYMENT_METHOD_NAME;
+    }
 }
